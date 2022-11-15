41cf8b0d623ecc1b762d3ff10982cc8b775d0fc9
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-15 09:39:14 -0800

    Add files via upload

---
+++ budget.go
@@ -0,0 +1,36 @@
+// Marvish Chandra
+
+package MyBudgetCategories
+
+import "fmt"
+
+func myHousing(monthlyRent,interest){
+	monthlyCost = monthlyRent * interest
+}
+
+func myTransportation(personalVehicle,rideShare){
+	totalTransportation = personalVehicle + rideShare
+}
+
+func foodCosts(groceries,takeout){
+	monthlyFood = groceries + takeout
+}
+
+func utilities(water,electrical,cable,internet,heating,cooling){
+	totalUtilities = water + electrical + cable + internet + heating + cooling
+}
+
+func insurance(life,vehicle,electronics,home){
+	totalInsurance = life + vehicle + electronics + home
+}
+
+func healthcare(individual,family){
+	totalHealth = individual + family
+}
+
+func debtPayments(loans, interest){
+	totalDebt = loans * interest
+}
+
+
+
